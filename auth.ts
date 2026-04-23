6320fd1ef78a0b2a74954a05fb8920af70ba40ed
Davi Silva <davi.pimenta8@gmail.com>
2026-04-23 15:02:56 -0300

    feat: implement authentication with NextAuth and middleware for dashboard access

---
+++ auth.ts
@@ -0,0 +1,44 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { prisma } from "@/lib/prisma";
+import { compare } from "bcryptjs";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Senha", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+
+        const clinic = await prisma.clinic.findUnique({
+          where: { email: credentials.email as string },
+        });
+
+        if (!clinic) {
+          return null;
+        }
+
+        const passwordMatch = await compare(
+          credentials.password as string,
+          clinic.password,
+        );
+
+        if (!passwordMatch) return null;
+
+        return {
+          id: clinic.id,
+          name: clinic.name,
+          email: clinic.email,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/auth",
+  },
+});
