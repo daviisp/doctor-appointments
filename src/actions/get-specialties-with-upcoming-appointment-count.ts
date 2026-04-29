b9d74e7b100456d85269500b177b3007e93f2f44
Davi Silva <davi.pimenta8@gmail.com>
2026-04-28 22:36:56 -0300

    feat: add getSpecialtiesWithUpcomingAppointmentCount function to retrieve specialties with upcoming appointment counts

---
+++ src/actions/get-specialties-with-upcoming-appointment-count.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { SpecialtyWithAppointmentCount } from "@/types/specialty-with-appointment-count";
+
+export const getSpecialtiesWithUpcomingAppointmentCount = async (): Promise<
+  SpecialtyWithAppointmentCount[]
+> => {
+  const now = new Date();
+
+  const specialties = await prisma.specialty.findMany({
+    include: {
+      doctors: {
+        include: {
+          _count: {
+            select: {
+              appointments: {
+                where: {
+                  date: {
+                    gte: now,
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const result = specialties.map((specialty) => ({
+    id: specialty.id,
+    name: specialty.name,
+    appointments: specialty.doctors.reduce(
+      (acc, doctor) => acc + doctor._count.appointments,
+      0,
+    ),
+  }));
+
+  const sorted = result
+    .sort((a, b) => b.appointments - a.appointments)
+    .slice(0, 5);
+  const max = sorted[0]?.appointments ?? 1;
+
+  return sorted.map((specialty) => ({
+    ...specialty,
+    progress: Math.round((specialty.appointments / max) * 100),
+  }));
+};
