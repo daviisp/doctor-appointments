9e8551fbfaa8757d16b4f85ecf1f6925164b0101
Davi Silva <davi.pimenta8@gmail.com>
2026-05-06 20:04:33 -0300

    feat: implement getDoctors function to retrieve doctor data with specialties and availabilities

---
+++ src/actions/get-doctors.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { Doctor } from "@/types/doctor";
+
+export const getDoctors = async (): Promise<Doctor[]> => {
+  return prisma.doctor.findMany({
+    include: {
+      specialty: {
+        select: {
+          name: true,
+        },
+      },
+      availabilities: {
+        select: {
+          weekDay: true,
+          startTime: true,
+          endTime: true,
+        },
+      },
+    },
+    orderBy: {
+      name: "asc",
+    },
+  });
+};
